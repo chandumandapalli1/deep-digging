52adc12fc2761a4c9ba9fe4adf453517a0879fe9
chandumandapalli1 <chandumandapalli1@gmail.com>
2022-11-12 12:55:40 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core"
+
+
+@Component(
+    {
+        selector:"my-app",
+        // template:`<h1> Hey Angular </h1>`,
+        templateUrl:"./app.component.html",
+        // styles:[`
+        // h1{
+        //     background-color:#FFF;
+        //     text-align:center
+        // }
+        
+        // `],
+        styleUrls:["./app.component.css"]
+        
+    }
+)
+export class  AppComponent
+{
+
+}
